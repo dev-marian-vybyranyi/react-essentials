71c9ecae3c110990ab64e1215818ed459a3c2971
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-29 13:31:39 +0200

    Implement editable player names in the Player component and update the initialName prop in App.jsx.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
-const Player = ({name, symbol}) => {
+import { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    };
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editableplayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editableplayerName = <input type="text" required value={playerName} onChange={handleChange} />
+    }
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editableplayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     )
 }
