6004507d2f056ddf4e814d36eeff238158d2d247
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-29 11:56:27 +0200

    Introduce `Header` component, refactor `App.jsx` to use it, and add initial project configuration files.

---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const reactDescription = ['Fundamental', "Crucial", "Core"];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+const Header = () => {
+    const description = reactDescription[genRandomInt(2)];
+
+    return (
+        <header>
+            <img src={reactImg} alt="Stylised Atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {description} React concepts you will need for almost any app you are going to build!
+            </p>
+        </header>
+    )
+}
+
+export default Header
